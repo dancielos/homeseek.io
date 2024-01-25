68395f6539768874cf1b2d67076aa402cdd3883c
Dan <iam@dancielos.com>
2024-01-24 20:47:06 -0700

    removed some unnecessary packages

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -10,9 +10,11 @@ describe('HomeSeek Homepage', () => {
 		cy.visit('http://localhost:3000');
 		cy.get('h1').should('exist', 'Hello World!');
 	});
-	it('should render 4 sections in the homepage', () => {
+	it('should render the crucial parts of the homepage', () => {
 		cy.visit('http://localhost:3000');
 		cy.get('section').should('have.length', '4');
+		cy.get('input#search').should('exist');
+		cy.get('input[type="submit"]').should('exist');
 	});
 	// it('should render the search input', () => {
 	// 	cy.
