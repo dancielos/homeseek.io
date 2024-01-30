167fddebd23df2ad95b770a19d87136b59532ba0
Dan <iam@dancielos.com>
2024-01-30 08:31:06 -0700

    added hover animation to CardCity

--- pages/Home/CardCity.tsx
+++ pages/Home/CardCity.tsx
@@ -6,6 +6,8 @@ import {
 } from '@mui/material';
 import Image from 'next/image';
 
+import styles from './CardCity.module.css';
+
 type cityProp = ImageListItemProps & {
 	item: City;
 	i: number;
@@ -20,26 +22,21 @@ export default function CardCity({
 		<ImageListItem
 			cols={i % 3 === 0 ? 2 : 1}
 			rows={i % 3 === 0 ? 2 : 1}
-			sx={{
-				alignItems: 'end',
-			}}
+			className={styles['img-container']}
 		>
 			<Image
 				src={item.img}
 				alt={item.title || 'test'}
 				width={640}
 				height={360}
-				style={{
-					width: '100%',
-					height: 'auto',
-				}}
+				className={styles['img']}
 				// fill
 				// style={{
 				// 	objectFit: 'contain',
 				// 	width: '100%',
 				// }}
 			/>
-			<ImageListItemBar title={item!.title} />
+			<ImageListItemBar title={item.title} />
 		</ImageListItem>
 	);
 }
