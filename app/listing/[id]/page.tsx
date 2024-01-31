edc10ba4902de135d763c833130b7ba545b5f4e7
Dan <iam@dancielos.com>
2024-01-31 13:17:47 -0700

    details' page title is done

--- app/listing/[id]/page.tsx
+++ app/listing/[id]/page.tsx
@@ -1,11 +1,59 @@
-import { AppBar } from '@mui/material';
+import H1 from '@/components/htmlElements/H1';
+import StickyBar from '@/components/ui/StickyBar';
+import { ArrowBackOutlined } from '@mui/icons-material';
+import { IconButton, Stack } from '@mui/material';
 
 export default function Details() {
 	return (
-		<div>
-			<AppBar position='sticky' color='secondary'>
-				Address here
-			</AppBar>
-		</div>
+		<>
+			<StickyBar
+				sx={{
+					pl: {
+						xm: 0,
+						sm: 1,
+					},
+					py: 0,
+					borderBottom: 'solid',
+					borderColor: 'secondary.main',
+				}}
+			>
+				<Stack flexDirection='row' columnGap={4}>
+					<IconButton
+						color='secondary'
+						sx={{
+							flexBasis: {
+								sm: '5%',
+								md: '10%',
+							},
+							py: 2,
+							borderRadius: 0,
+							borderRight: 'solid',
+						}}
+					>
+						<ArrowBackOutlined />
+					</IconButton>
+
+					<H1
+						sx={{
+							letterSpacing: 1,
+							fontSize: {
+								// xm: '1rem',
+								xs: '1.2rem',
+								xm: '1.4rem',
+								sm: '1.8rem',
+								md: '2rem',
+							},
+							p: 0,
+							py: {
+								xs: 1,
+								sm: 2,
+							},
+						}}
+					>
+						150 Peter Herner Bay, Winnipeg, Manitoba, R2V 4W5
+					</H1>
+				</Stack>
+			</StickyBar>
+		</>
 	);
 }
--- components/ui/StickyBar.tsx
+++ components/ui/StickyBar.tsx
@@ -1,19 +1,27 @@
-import { AppBar } from '@mui/material';
+import { AppBar, AppBarProps, SxProps } from '@mui/material';
 import { ReactNode } from 'react';
 
-export default function StickyBar({ children }: { children: ReactNode }) {
+type StickyBarProps = {
+	children: ReactNode;
+	sx?: SxProps;
+};
+
+export default function StickyBar({ children, sx }: StickyBarProps) {
+	let customStyle: SxProps = {
+		bgcolor: '#fff4e69e',
+		boxShadow: 'none',
+		py: {
+			xs: 1,
+			sm: 2,
+		},
+	};
+	if (sx) customStyle = { ...customStyle, ...sx };
+
 	return (
 		<AppBar
 			position='sticky'
 			// color='#fff4e6'
-			sx={{
-				bgcolor: '#fff4e69e',
-				boxShadow: 'none',
-				py: {
-					xs: 1,
-					sm: 2,
-				},
-			}}
+			sx={customStyle}
 			elevation={0}
 		>
 			{children}
