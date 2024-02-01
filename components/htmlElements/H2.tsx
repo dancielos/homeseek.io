6354435cf6272e81ea2bc6e8a6b322acfd1bc9a2
Dan <iam@dancielos.com>
2024-01-31 20:28:38 -0700

    designed the features amenities section

--- components/htmlElements/H2.tsx
+++ components/htmlElements/H2.tsx
@@ -1,19 +1,30 @@
-import { Typography } from '@mui/material';
+import { Typography, TypographyProps } from '@mui/material';
 import React from 'react';
 
-export default function H2({ children }: { children: React.ReactNode }) {
-	return (
-		<Typography
-			component='h2'
-			variant='h2'
-			sx={{
+interface H2Props extends TypographyProps {
+	children: React.ReactNode;
+	smaller?: boolean;
+}
+
+export default function H2({ children, smaller = false, ...args }: H2Props) {
+	const customStyle = smaller
+		? {
+				fontSize: {
+					xs: '1.2rem',
+					sm: '1.8rem',
+					md: '2rem',
+				},
+		  }
+		: {
 				fontSize: {
 					xs: '2rem',
 					sm: '2.5rem',
 					md: '3rem',
 				},
-			}}
-		>
+		  };
+
+	return (
+		<Typography component='h2' variant='h2' sx={customStyle} {...args}>
 			{children}
 		</Typography>
 	);
--- pages/Details/FeaturesAmenities.tsx
+++ pages/Details/FeaturesAmenities.tsx
@@ -2,7 +2,7 @@
 
 import H2 from '@/components/htmlElements/H2';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { Box, Tab } from '@mui/material';
+import { Box, Stack, Tab } from '@mui/material';
 import { useState } from 'react';
 
 function createData(
@@ -30,21 +30,33 @@ export default function FeaturesAmenities() {
 	};
 	return (
 		<>
-			<H2>Features & Amenities</H2>
-			<TabContext value={value}>
-				<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-					<TabList onChange={handleChange} aria-label='lab API tabs example'>
-						<Tab label='All' value='1' />
-						<Tab label='Features' value='2' />
-						<Tab label='Nearby' value='3' />
-						<Tab label='Other' value='4' />
-					</TabList>
-				</Box>
-				<TabPanel value='1'>Item One</TabPanel>
-				<TabPanel value='2'>Item Two</TabPanel>
-				<TabPanel value='3'>Item Three</TabPanel>
-				<TabPanel value='4'>Item Three</TabPanel>
-			</TabContext>
+			<Stack>
+				<H2 smaller>Features & Amenities</H2>
+				<TabContext value={value}>
+					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+						<TabList
+							textColor='inherit'
+							indicatorColor='secondary'
+							variant='scrollable'
+							onChange={handleChange}
+							aria-label='lab API tabs example'
+							sx={{
+								color: '#212529',
+								backgroundColor: 'primary.light',
+							}}
+						>
+							<Tab label='All' value='1' />
+							<Tab label='Features' value='2' />
+							<Tab label='Nearby' value='3' />
+							<Tab label='Other' value='4' />
+						</TabList>
+					</Box>
+					<TabPanel value='1'>Item One</TabPanel>
+					<TabPanel value='2'>Item Two</TabPanel>
+					<TabPanel value='3'>Item Three</TabPanel>
+					<TabPanel value='4'>Others</TabPanel>
+				</TabContext>
+			</Stack>
 		</>
 	);
 }
