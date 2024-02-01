f382ea36943e54746915dfe3f23ea12c4aab35be
Dan <iam@dancielos.com>
2024-01-31 20:30:02 -0700

    designed utilities section

--- pages/Details/UtilitiesIncluded.tsx
+++ pages/Details/UtilitiesIncluded.tsx
@@ -1,15 +1,18 @@
 import H2 from '@/components/htmlElements/H2';
 import { Chip, Stack } from '@mui/material';
 
+const arr = [1, 2, 3, 4];
+
 export default function UtilitiesIncluded() {
 	return (
 		<>
-			<H2>Utilities Included</H2>
-			<Stack flexDirection='row' gap={1}>
-				<Chip label='Lorem' />
-				<Chip label='Lorem' />
-				<Chip label='Lorem' />
-				<Chip label='Lorem' />
+			<Stack>
+				<H2 smaller>Utilities Included</H2>
+				<Stack flexDirection='row' gap={1}>
+					{arr.map((a) => (
+						<Chip label={`Lorem${a}`} />
+					))}
+				</Stack>
 			</Stack>
 		</>
 	);
