e5bcdb9f525c864629ab2ae7e50a43ba29cd69d5
Dan <iam@dancielos.com>
2024-02-02 08:25:59 -0700

    moved the whole dashboard layout to layout.tsx from page.tsx

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,11 +1,31 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+
+import Box from '@mui/material/Box';
+
 import AdminAppBar from '@/layouts/admin/AdminAppBar';
-import { ReactNode } from 'react';
+import AdminDrawer from '@/layouts/admin/AdminDrawer';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
+	const [open, setOpen] = useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
 	return (
 		<>
-			{/* <AdminAppBar /> */}
-			<main>{children}</main>
+			<Box sx={{ display: 'flex' }}>
+				<AdminAppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+				<AdminDrawer open={open} handleDrawerClose={handleDrawerClose} />
+				<Box component='main' sx={{ flexGrow: 1, p: 3, pt: 8 }}>
+					{children}
+				</Box>
+			</Box>
 		</>
 	);
 }
