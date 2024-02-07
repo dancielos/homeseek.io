64e3d1386522b4f1cd3a375530f16899cd3d385d
Dan <iam@dancielos.com>
2024-02-06 21:07:24 -0700

    added the RED portion of interlink testing

---
+++ cypress/e2e/admin/links.cy.ts
@@ -0,0 +1,55 @@
+describe('Links should direct the user to correct pages', () => {
+	const homepageRegex = new RegExp('^' + Cypress.config().baseUrl + '/?$');
+
+	it('should navigate to login page and then to dashboard', () => {
+		cy.visit(`${Cypress.config().baseUrl}`);
+		cy.get('a#login-link').click();
+		cy.url().should('include', '/login');
+
+		cy.get('[type=submit]').click();
+		cy.url().should('include', '/dashboard');
+	});
+
+	it('should navigate to homepage from dashboard', () => {
+		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
+		cy.get('a#drawer-link-homepage').click();
+		cy.url().should('match', homepageRegex);
+	});
+
+	it('should navigate to messages page', () => {
+		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
+		cy.get('a#drawer-link-messages').click();
+		cy.url().should('include', '/messages');
+	});
+
+	it('should navigate to properties page', () => {
+		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
+		cy.get('a#drawer-link-properties').click();
+		cy.url().should('include', '/properties');
+	});
+
+	it('should navigate to add new property page', () => {
+		cy.visit(`${Cypress.config().baseUrl}/properties`);
+		cy.get('a#add-new-property-link').click();
+		cy.url().should('include', '/properties/new');
+	});
+
+	it('should navigate back to dashboard from footer', () => {
+		cy.visit(`${Cypress.config().baseUrl}`);
+		cy.get('footer').find('a#footer-link-post-listing').click();
+		cy.url().should('include', '/dashboard');
+
+		cy.get('a#drawer-link-homepage').click();
+		cy.url().should('match', homepageRegex);
+	});
+
+	it('should navigate to login page from footer', () => {
+		cy.visit(`${Cypress.config().baseUrl}`);
+		cy.get('footer').find('a#footer-link-login').click();
+		cy.url().should('include', '/login');
+
+		// Assuming the login page has a submit button [type=submit]
+		cy.get('[type=submit]').click();
+		cy.url().should('include', '/dashboard');
+	});
+});
