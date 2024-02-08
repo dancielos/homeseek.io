fb2edd5c78b38ec7d30db00484cc38508ad5d3b8
Dan <iam@dancielos.com>
2024-02-08 08:09:18 -0700

    unit test for TitleCase() is now GREEN.

--- utils/toTitleCase.ts
+++ utils/toTitleCase.ts
@@ -1,3 +1,12 @@
 export default function toTitleCase(text: string) {
-	return '';
+	const words = text.split(' ');
+	return words
+		.map((word) => {
+			let newWord = '';
+			for (let i = 0; i < word.length; i++) {
+				newWord += i === 0 ? word[i].toUpperCase() : word[i].toLowerCase();
+			}
+			return newWord;
+		})
+		.join(' ');
 }
