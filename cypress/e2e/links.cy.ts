e40ae28735131420eef59b81c69fc9438d836370
Dan <iam@dancielos.com>
2024-02-22 19:10:15 -0700

    re-installed cypress

--- cypress/e2e/links.cy.ts
+++ cypress/e2e/links.cy.ts
@@ -3,33 +3,33 @@ import { CycloneTwoTone } from '@mui/icons-material';
 describe('Links should direct the use to correct pages', () => {
 	// it('must have ')
 	const homepageRegex = new RegExp('^' + Cypress.config().baseUrl + '/?$');
-	it('should link to correct pages', () => {
-		cy.visit(`${Cypress.config().baseUrl}`);
-		cy.get('a#search-submit').click();
+	// it('should link to correct pages', () => {
+	// 	cy.visit(`${Cypress.config().baseUrl}`);
+	// 	cy.get('a#search-submit').click();
 
-		cy.url().should('include', '/search');
-		cy.get('a#homeseek-logo').click();
+	// 	cy.url().should('include', '/search');
+	// 	cy.get('a#homeseek-logo').click();
 
-		cy.url().should('match', homepageRegex);
-		cy.get('a[data-testid=more-details-0]').click();
-		cy.wait(5000);
+	// 	cy.url().should('match', homepageRegex);
+	// 	cy.get('a[data-testid=more-details-0]').click();
+	// 	cy.wait(5000);
 
-		cy.url().should('include', '/listing/');
-		cy.get('[data-testid=go-back]').click();
+	// 	cy.url().should('include', '/listing/');
+	// 	cy.get('[data-testid=go-back]').click();
 
-		cy.url().should('match', homepageRegex);
-		cy.get('a[data-testid=listing-for-city-toronto]').click();
+	// 	cy.url().should('match', homepageRegex);
+	// 	cy.get('a[data-testid=listing-for-city-toronto]').click();
 
-		cy.url().should('include', '/search?query=toronto');
-		cy.get('a[data-testid=more-details-0]').click();
+	// 	cy.url().should('include', '/search?query=toronto');
+	// 	cy.get('a[data-testid=more-details-0]').click();
 
-		cy.url().should('include', '/listing/');
-		cy.get('[data-testid=go-back]').click();
+	// 	cy.url().should('include', '/listing/');
+	// 	cy.get('[data-testid=go-back]').click();
 
-		cy.url().should('include', '/search');
-		cy.get('a#homeseek-logo').click();
-		cy.url().should('match', homepageRegex);
-	});
+	// 	cy.url().should('include', '/search');
+	// 	cy.get('a#homeseek-logo').click();
+	// 	cy.url().should('match', homepageRegex);
+	// });
 	it.only('testing the listing', () => {
 		cy.visit(`${Cypress.config().baseUrl}/listing/toronto`);
 		cy.url().should('include', '/listing/');
