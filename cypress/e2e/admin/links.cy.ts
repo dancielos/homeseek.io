81986cdfa4a78e2e9204eab76c9b2218a8ccfbe9
Dan <iam@dancielos.com>
2024-02-28 18:11:38 -0700

    fixed some e2e tests

--- cypress/e2e/admin/links.cy.ts
+++ cypress/e2e/admin/links.cy.ts
@@ -1,55 +1,52 @@
 describe('Links should direct the user to correct pages', () => {
 	const homepageRegex = new RegExp('^' + Cypress.config().baseUrl + '/?$');
 
-	it('should navigate to login page and then to dashboard', () => {
+	it('should navigate around the admin pages', () => {
 		cy.visit(`${Cypress.config().baseUrl}`);
 		cy.get('a#navbar-link-login').click();
 		cy.url().should('include', '/login');
 
-		cy.get('[type=submit]').click();
+		cy.get('form#login-form').within(() => {
+			cy.get('input[name=email]').type('demo@homeseek.io');
+			cy.get('input[name=password]').type('Password123!');
+			cy.get('button[type=submit]').click();
+		});
 		cy.url().should('include', '/dashboard');
-	});
 
-	it('should navigate to dashboard from homepage (CTA)', () => {
+		// should navigate to dashboard from homepage (CTA)
 		cy.visit(`${Cypress.config().baseUrl}`);
 		cy.get('a#navbar-link-post-listing').click();
 		cy.url().should('include', '/dashboard');
-	});
 
-	it('should navigate to homepage from dashboard', () => {
+		// should navigate to homepage from dashboard
 		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
 		cy.get('a#drawer-link-homepage').click();
 		cy.url().should('match', homepageRegex);
-	});
 
-	it('should navigate to messages page', () => {
+		// should navigate to messages page
 		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
 		cy.get('a#drawer-link-messages').click();
 		cy.url().should('include', '/messages');
-	});
 
-	it('should navigate to properties page', () => {
+		// should navigate to properties page
 		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
 		cy.get('a#drawer-link-properties').click();
 		cy.url().should('include', '/properties');
-	});
 
-	it('should navigate to properties page from dashboard reports', () => {
+		// should navigate to properties page from dashboard reports
 		cy.visit(`${Cypress.config().baseUrl}/dashboard`);
 		cy.get('a#dashboard-manage-properties').click();
 		cy.url().should('include', '/properties');
-	});
 
-	it('should navigate to add new property page then back to dashboard', () => {
+		// should navigate to add new property page then back to dashboard
 		cy.visit(`${Cypress.config().baseUrl}/properties`);
 		cy.get('a#add-new-property-link').click();
 		cy.url().should('include', '/properties/new');
 
 		cy.get('a#drawer-link-dashboard').click();
 		cy.url().should('include', '/dashboard');
-	});
 
-	it('should navigate back to dashboard from footer', () => {
+		// should navigate back to dashboard from footer
 		cy.visit(`${Cypress.config().baseUrl}`);
 		cy.get('footer').find('a#footer-link-post-listing').click();
 		cy.url().should('include', '/dashboard');
@@ -62,9 +59,5 @@ describe('Links should direct the user to correct pages', () => {
 		cy.visit(`${Cypress.config().baseUrl}`);
 		cy.get('footer').find('a#footer-link-login').click();
 		cy.url().should('include', '/login');
-
-		// Assuming the login page has a submit button [type=submit]
-		cy.get('[type=submit]').click();
-		cy.url().should('include', '/dashboard');
 	});
 });
