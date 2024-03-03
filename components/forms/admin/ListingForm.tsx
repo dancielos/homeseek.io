548cb99143d8d93ac708802ddc71ac8e0d6ea199
Dan <iam@dancielos.com>
2024-03-03 10:36:42 -0700

    added a hidden input for autocomplete features

--- components/forms/admin/ListingForm.tsx
+++ components/forms/admin/ListingForm.tsx
@@ -8,14 +8,75 @@ import Agreement from './Agreement';
 import AddressBasicDetails from './AddressBasicDetails';
 import FeaturesAmenitiesUtilities from './FeaturesAmenitiesUtilities';
 import ImageUpload from './ImageUpload';
+import { useState } from 'react';
+import { useDebounce } from '@/hooks/useDebounce';
+import { useFormState } from 'react-dom';
+
+const AMENITIES_FEATURES = [
+	'On-Site Staff',
+	'Fitness Area',
+	'Laundry Facilities',
+	'Parking - Underground',
+	'Elevator',
+	'Storage Lockers',
+	'Recreation Room',
+	'Swimming Pool',
+	'Balcony',
+	'Central Heating',
+	'Air Conditioning',
+	'Pet Friendly',
+	'Wheelchair Accessible',
+	'Gymnasium',
+	'Roof Deck',
+	'24/7 Security',
+	'Concierge Service',
+	'BBQ Area',
+	'Garden',
+	'Childcare Facility',
+];
+
+const AMENITIES_NEARBY = [
+	'Schools',
+	'Shopping',
+	'Convenience Store',
+	'Public Transit',
+	'Parks',
+	'Restaurants',
+	'Pharmacy',
+	'Gym/Fitness Center',
+	'Hospital/Clinic',
+	'Library',
+];
 
 export default function ListingForm({
 	id = 'new-property-form',
 }: {
 	id: string;
 }) {
+	const [valueFeatures, setValueFeatures] = useState<string[]>([
+		AMENITIES_FEATURES[0],
+	]);
+
+	// const debouncedFeatures = useDebounce(valueFeatures);
+
+	async function test(prevState: string, formData: FormData) {
+		console.log(Array.from(formData.entries()));
+		// console.log({ debouncedFeatures });
+		return '';
+	}
+
+	// const test2 = test.bind(null, { features: debouncedFeatures });
+	const [response, formAction] = useFormState(test, '');
+
 	return (
-		<Grid container columns={10} spacing={1} component='form' id={id}>
+		<Grid
+			container
+			columns={10}
+			spacing={1}
+			component='form'
+			id={id}
+			action={formAction}
+		>
 			<Grid xs={10} md={5}>
 				<AddressBasicDetails />
 			</Grid>
@@ -28,7 +89,12 @@ export default function ListingForm({
 					gap: 2,
 				}}
 			>
-				<FeaturesAmenitiesUtilities />
+				<input type='hidden' name='amenities-features' value={valueFeatures} />
+				<FeaturesAmenitiesUtilities
+					featuresOptions={AMENITIES_FEATURES}
+					valueFeatures={valueFeatures}
+					setValueFeatures={setValueFeatures}
+				/>
 				<ImageUpload />
 			</Grid>
 
