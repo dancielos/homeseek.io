ca9797aab4416e2bc5e2cee7f57f60f140bcd5be
Dan <iam@dancielos.com>
2024-03-08 17:16:59 -0700

    created a delete function for images

---
+++ utils/server-actions/deleteFromS3.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { DeleteObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+export default async function deleteFromS3(
+	images: Array<string>
+): Promise<boolean> {
+	console.log('deleting from s3');
+	if (images.length === 0) return false;
+
+	const client = new S3Client({
+		region: process.env.BUCKET_REGION,
+	});
+	const commands = images.map((image) => {
+		return client.send(
+			new DeleteObjectCommand({
+				Bucket: process.env.BUCKET_NAME,
+				Key: image,
+			})
+		);
+	});
+
+	const responses = await Promise.all(commands);
+
+	return responses.every(
+		(response) => response['$metadata'].httpStatusCode === 200
+	);
+}
